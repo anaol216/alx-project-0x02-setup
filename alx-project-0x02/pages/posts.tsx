cdf3e1c0ff8fc8ae24566ada562c149e7a5974d4
anaol216 <atinafuanaol@gmail.com>
2025-10-04 12:11:04 +0300

    modified:   alx-project-0x02/pages/posts.tsx

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -3,6 +3,7 @@ import Header from '../components/layout/Header';
 export const Posts: React.FC = () => {
     return (
         <div>
+            <Header />
             <h1>Posts Page</h1>
             <p>This is a sample posts page component.</p>   
         </div>
