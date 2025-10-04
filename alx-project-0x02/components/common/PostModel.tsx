57b64d80ed29ee25e5ea4b70a96fda1d9b9d96af
anaol216 <atinafuanaol@gmail.com>
2025-10-04 11:41:56 +0300

    feat: add PostModel component and integrate it into the Home page

---
+++ alx-project-0x02/components/common/PostModel.tsx
@@ -0,0 +1,11 @@
+import { CardProps } from "@/interfaces";
+
+
+export const PostModel: React.FC<CardProps> = () => {
+    return (
+        <div>
+            <h1>Post Model Component</h1>   
+            <p>This is a sample post model component.</p>
+        </div>
+    );
+}
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,9 +1,11 @@
 import Card from "@/components/common/Card"
+import { PostModel } from "@/components/common/PostModel";
 export const Home: React.FC = () => {
     return (
         <div>
             <h1>Welcome to the Home Page</h1>
             <Card title="Sample Card" content="This is a sample card component." />
+            <PostModel title="hello ther" content="here we go"/>
         </div>
     );
 }
