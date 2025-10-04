551b68716f2506092d6a51b80179e077dc43f8ea
anaol216 <atinafuanaol@gmail.com>
2025-10-04 11:28:28 +0300

    feat: implement Card component with title and content display

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,10 @@
+
+
+export const Card: React.FC<CardProps> = ({ title, content }) => {
+    return (
+        <div className="card">
+            <h2>{title}</h2>
+            <p>{content}</p>
+        </div>
+    );
+}                 
